110dc8b48280a59d73d2b8a6839cefde1b823e7f
ICB Yuge Ji <jiyuge@gmail.com>
2022-09-09 23:17:36 +0200

    test code file

--- scripts/de_utils.py
+++ scripts/de_utils.py
@@ -15,49 +15,6 @@ def enrich(query, background, return_full=False, organism='hsapiens', sources=['
     else:
         return df[['name', 'p_value', 'intersections']]
 
-# Enrichr setup
-import json
-import requests
-
-def enrichr(genes, library='KEGG_2019_Human'):
-    ENRICHR_URL = 'http://maayanlab.cloud/Enrichr/addList'
-    genes_str = '\n'.join(list(genes))
-    # name of analysis or list
-    description = 'enrichment'
-    
-    ##run enrichment
-    payload = {'list': (None, genes_str), 'description': (None, description)}
-    response = requests.post(ENRICHR_URL, files=payload)
-    if not response.ok:
-        raise Exception('Error analyzing gene list')
-    job_id = json.loads(response.text)
-
-    ## get enrichment results
-    ENRICHR_URL = 'http://maayanlab.cloud/Enrichr/enrich'
-    query_string = '?userListId=%s&backgroundType=%s'
-    response = requests.get(
-        ENRICHR_URL + query_string % (str(job_id['userListId']), library)
-     )
-    if not response.ok:
-        raise Exception('Error fetching enrichment results')
-    print('Enrichr API : Get enrichment results: Job Id:', job_id)
-    
-    # construct readable dataframe
-    out = pd.DataFrame(list(json.loads(response.text).values())[0])
-    out.columns = [
-        'Rank',
-        'Term name',
-        'P-value',
-        'Z-score',
-        'Combined score',
-        'Overlapping genes',
-        'Adjusted p-value',
-        'Old p-value',
-        'Old adjusted p-value'
-    ]
-    return out
-
-
 import scipy
 import numpy as np
 import matplotlib.pyplot as plt
