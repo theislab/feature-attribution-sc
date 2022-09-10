145860bc6e03d9b4e08bcd8af0e7e9522a6835a4
ICB Yuge Ji <jiyuge@gmail.com>
2022-09-10 18:52:48 +0200

    test formatting

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,16 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import sys
+from pathlib import Path
+from datetime import datetime
+
+import matplotlib
+matplotlib.use('agg')
+
+HERE = Path(__file__).parent
+sys.path.insert(0, str(HERE.parent))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -13,7 +23,12 @@ author = 'Theislab team 8'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.coverage',
+    'sphinx.ext.autosummary'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +38,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "classic"
+#html_static_path = ['_static']
+html_theme_options = {
+    "rightsidebar": "true",
+    "relbarbgcolor": "black"
+}
--- docs/index.rst
+++ docs/index.rst
@@ -1,15 +1,12 @@
-.. feature-attribution-sc documentation master file, created by
-   sphinx-quickstart on Fri Sep  9 23:14:58 2022.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Welcome to feature-attribution-sc's documentation!
-==================================================
+feature attribution toolkit
+===========================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
+   notebooks
+   scripts
 
 
 Indices and tables
