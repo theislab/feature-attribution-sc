98749379c82cb15509e182d4718624e7a576826f
zethson <lukas.heumos@posteo.net>
2022-11-29 13:41:14 +0100

    Add run time measurements draft
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,2 +1,3 @@
 from scripts.de_utils import enrich, plot_correlation
 from scripts.benchmarking import hlca_markers, roc_auc_hlca, roc_auc_crispr
+from scripts.run_time_measurements import calculate_runtime, calculate_runtime_dec
