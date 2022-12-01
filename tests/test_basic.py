441c243f8fcd2d7544d70ca2ab29fb4e4df7f772
zethson <lukas.heumos@posteo.net>
2022-12-01 18:04:40 +0100

    Remove test
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,11 +1,5 @@
 import pytest
 
-import feature_attribution_sc
-
-
-def test_package_has_version():
-    feature_attribution_sc.__version__
-
 
 @pytest.mark.skip(reason="This decorator should be removed when test passes.")
 def test_example():
