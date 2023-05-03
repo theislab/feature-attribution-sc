ff60bd1b6a75477dafe5482ceede3d78e6beac86
Sergei Rybakov <koncopd@gmail.com>
2023-05-03 14:51:45 +0200

    Add neutral input inference for scanvi (#13)
    * add neutral input
    
    * temporary notebook
    
    * add custom inpt

---
+++ feature_attribution_sc/explainers/neutral_input.py
@@ -0,0 +1,38 @@
+from copy import deepcopy
+
+import torch
+
+
+def neutral_input(scanvi_model, epochs=2000, inpt=None, size_factor=None, batch_index=None, **kwargs):
+    scanvi_model = deepcopy(scanvi_model)
+    for param in scanvi_model.module.parameters():
+        param.requires_grad = False
+
+    if batch_index is not None and not isinstance(batch_index, torch.Tensor):
+        batch_index = torch.tensor(batch_index, dtype=torch.float32, device=param.device).reshape((1, 1))
+
+    n_vars = scanvi_model.summary_stats["n_vars"]
+    if inpt is None:
+        inpt = torch.ones((1, n_vars), device=param.device)
+        if size_factor is None:
+            inpt = inpt * 0.0
+        else:
+            inpt = inpt / n_vars
+            inpt = torch.distributions.Dirichlet(inpt).sample() * size_factor
+    else:
+        inpt = torch.tensor(inpt, dtype=torch.float32, device=param.device)
+    inpt.requires_grad = True
+
+    optim = torch.optim.Adam(params=(inpt,), **kwargs)
+
+    for i in range(epochs):
+        optim.zero_grad()
+        probs = scanvi_model.module.classify(x=inpt, batch_index=batch_index)
+        if i % 100 == 0:
+            print(f"Epoch {i}: max probability - {probs.max().item()}.")
+        entropy = torch.distributions.Categorical(probs=probs).entropy()
+        (-entropy).backward()
+        optim.step()
+        inpt.data.clamp_(0.0)
+
+    return inpt.detach()
